4df7b53ea569da725c19470f51300932691bd50f
Dionisio <benevidescursos@gmail.com>
2023-06-23 13:13:23 -0300

    fix: solving problem with schema

--- src/interfaces/cars.interfaces.ts
+++ src/interfaces/cars.interfaces.ts
@@ -1,10 +1,14 @@
 import { DeepPartial } from "typeorm";
 import { z } from "zod";
-import { carCreateSchema, returnAllCarsSchema, returnCarSchema } from "../schemas/cars.schema";
+import {
+  carCreateSchema,
+  returnAllCarsSchema,
+  returnCarAndUserSchema,
+  returnCarSchema,
+} from "../schemas/cars.schema";
 
 export type ICar = z.infer<typeof carCreateSchema>;
 export type ICarReturn = z.infer<typeof returnCarSchema>;
 export type IUpdateCar = DeepPartial<z.infer<typeof carCreateSchema>>;
 export type IReturnAllCars = z.infer<typeof returnAllCarsSchema>;
-// export type ICarAndUserReturn = z.infer<typeof returAllInfoCarAndUserSchema>;
-// export type IReturnAllCarsUser = z.infer<typeof returnCarAndUserSchema>;
+export type IReturnAllCarsUser = z.infer<typeof returnCarAndUserSchema>;
--- src/schemas/cars.schema.ts
+++ src/schemas/cars.schema.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { imageCreateSchema } from "./image.schema";
 import { fuel } from "../entities/cars.entities";
+import { returnUserSchema, returnUserSchemaWithOutAdress } from "./users.schema";
 
 export const carCreateSchema = z.object({
   brand: z.string().min(1).max(100),
@@ -19,31 +20,12 @@ export const carCreateSchema = z.object({
 
 export const returnCarSchema = carCreateSchema.extend({
   id: z.string().uuid(),
+  createdAt: z.string(),
 });
 
-// export const returAllInfoCarAndUserSchema = z.object({
-//   id: z.string().uuid(),
-//   brand: z.string().min(1).max(100),
-//   model: z.string().min(1).max(100),
-//   year: z.string(),
-//   typeCar: z.nativeEnum(fuel),
-//   mileage: z.number().positive(),
-//   color: z.string().min(1).max(100),
-//   fipePrice: z.number().positive(),
-//   price: z.number().positive(),
-//   description: z.string().optional().nullable(),
-//   imageCover: z.string().min(1).max(250),
-//   isAvailable: z.boolean().optional().nullable(),
-//   images: imageCreateSchema.array(),
-//   user: testSchema,
-// });
-
-// export const returnCarAndUserSchema = carCreateSchema
-//   .extend({
-//     id: z.string().uuid(),
-//     user: returnPersonalizeUserSchema,
-//   })
-//   .array();
+export const returnCarAndUserSchema = returnCarSchema.extend({
+  user: returnUserSchemaWithOutAdress,
+});
 
 export const returnAllCarsSchema = returnCarSchema.array();
 

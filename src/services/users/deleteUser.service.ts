0fff12f709653b80603cba53f7d41e3740652d81
Dionisio <benevidescursos@gmail.com>
2023-07-06 15:05:22 -0300

    hotFix: adjusting logic in delete user

--- src/services/users/deleteUser.service.ts
+++ src/services/users/deleteUser.service.ts
@@ -2,6 +2,7 @@ import { Repository } from "typeorm";
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities";
 import { deleteCarService } from "../cars/deleteCar.service";
+import { deleteCommentService } from "../comments/deleteComments.service";
 
 export const deleteUserService = async (id: string): Promise<void> => {
   const userRepository: Repository<User> = AppDataSource.getRepository(User);
@@ -11,13 +12,18 @@ export const deleteUserService = async (id: string): Promise<void> => {
       id: id,
     },
     relations: {
-      announcement: true,
+      comments: true,
     },
   });
-
-  if (user?.announcement?.length! > 0) {
+  if (user?.announcement) {
     user?.announcement?.forEach(async (car) => {
       await deleteCarService(car.id);
+    });
+  }
+
+  if (user?.comments) {
+    user?.comments.forEach(async (comment) => {
+      await deleteCommentService(comment.id);
     });
   }
 
